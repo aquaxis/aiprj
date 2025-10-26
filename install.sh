4c914e6aa60ff326108091b3083c827161f3549f
Hidemi Ishihara <hidemi@aquaxis.com>
2025-10-26 15:58:13 +0900

    update

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ cp -r .gemini ~/.local/share/aiprj/
 
 cp .gitignore.org ~/.local/share/aiprj/
 cp .mcp.json ~/.local/share/aiprj/
-cp AI_* ~/.local/share/aiprj/
 
 cp aiprj ~/.local/bin/aiprj
 chmod 755 ~/.local/bin/aiprj
